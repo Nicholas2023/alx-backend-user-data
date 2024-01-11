2f43b812db9d9e8ed90c3814f999ce7b847fb537
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-11 04:33:37 +0300

    task3

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -9,6 +9,7 @@ RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 filter_datum = __import__('filtered_logger').filter_datum
 get_logger = __import__('filtered_logger').get_logger
 PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+get_db = __import__('filtered_logger').get_db
 
 
 fields = ["password", "date_of_birth"]
@@ -27,3 +28,12 @@ print(formatter.format(log_record))
 
 print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
+
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close
---
+++ 0x00-personal_data/main.sql
@@ -0,0 +1,16 @@
+-- setup mysql server
+-- configure permissions
+
+CREATE DATABASE IF NOT EXISTS my_db;
+CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON my_db.* TO 'root'@'localhost';
+
+USE my_db;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	email VARCHAR(256)
+);
+
+INSERT INTO users(email) VALUES ("nicholasodhiambo2015@gmail.com");
+INSERT INTO users(email) VALUES ("muturianna@gmail.com");
