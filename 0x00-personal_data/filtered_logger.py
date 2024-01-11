f6c98a1a971f4f9b018cd2a0671619a9593cc479
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-11 03:33:12 +0300

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""A module for filtering logs.
 """
-A module that filters personal information data
-"""
+import os
 import re
+import logging
+import mysql.connector
 from typing import List
 
 
@@ -13,17 +15,10 @@ patterns = {
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
-    """
-    Obfuscate specified fields in the log message
-    Args:
-        fields (list[str]) : List of str to obfuscate
-        redaction (str) : The value to use in obfuscation
-        message (str): String representing the log line
-        separator (str) : Str representing character seperating fields
-    Returns:
-        str : Obfuscated log message
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
     """
     extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, seperator), replace(redaction), message)
+    return re.sub(extract(fields, separator), replace(redaction), message)
