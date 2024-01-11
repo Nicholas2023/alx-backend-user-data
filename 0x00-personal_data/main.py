1f7b150b794227e6d5137ef2dcc24168e010de05
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-11 04:07:06 +0300

    task2

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -7,6 +7,8 @@ import re
 
 RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 filter_datum = __import__('filtered_logger').filter_datum
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
 
 fields = ["password", "date_of_birth"]
@@ -21,3 +23,7 @@ message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
 log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
 formatter = RedactingFormatter(fields=("email", "ssn", "password"))
 print(formatter.format(log_record))
+
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
