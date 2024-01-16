206e4d146c84f55d41441391f323bbbd5d6b6b14
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-16 04:36:16 +0300

    task1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,19 +23,43 @@ elif AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """
-    Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> str:
+    '''Before request handler requests validation'''
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
